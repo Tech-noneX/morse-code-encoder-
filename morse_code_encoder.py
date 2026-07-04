9a327ce7344bef8f14c85d855294cae8a6df6661
Teknon766 <honzaander@gmail.com>
2026-07-04 12:08:50 +0100

    Updated of new stile since now im going write code like that

--- morse_code_encoder.py
+++ morse_code_encoder.py
@@ -1,5 +1,7 @@
 
-letters_to_morse = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D':'-..',
+
+
+letters_to_morse: dict[str, str] = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D':'-..',
                  'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
                  'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
                  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
@@ -7,48 +9,49 @@ letters_to_morse = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D':'-..',
                  'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
                  'Y': '-.--', 'Z': '--..',
 }
-def morse_menu():
+def morse_menu() -> None: 
     while True:   
         print('Option 1: Text to morse')
         print('Option 2: Morse to text')
         print('for Option 1 enter 1, for option 2 enter 2.')
         print('To quit enter q')
-        user_option = input(':')
+        user_option: str = input(':').lower().strip()
         if user_option == '1':
-            user_text = input('Enter your text.\n:')
-            text_to_morse(user_text)
+            user_text: str = input('Enter your text.\n:').upper()
+            print(text_to_morse(user_text))
         elif user_option == '2':
             print("each code separate by 'space', words separate by '/'")
-            user_morse = input('Enter morse code')
-            morse_to_text(user_morse)
+            user_morse: str = input('Enter morse code').upper()
+            print(morse_to_text(user_morse))
         elif user_option == 'q':
             break
         else:
             print('entered input not recognized')
             continue
-def morse_to_text(user_morse):
+def morse_to_text(user_morse) -> str:
     
-    translation =[]
+    translation: list[str] = []
     for i in user_morse:
-        for key, value in letters_to_morse.items():
-            if value == i:
-                translation.append(key)
+        for letter, morse_code in letters_to_morse.items():
+            if morse_code == i:
+                translation.append(letter)
         
 
 
-def text_to_morse(user_text):
+def text_to_morse(user_text) -> str:
     
-    translation = []
+    translation: list[str] = []
     for i in user_text:
         i = i.upper()
         if i in letters_to_morse:
             translation.append(letters_to_morse.get(i))
         elif i == ' ':
             translation.append('/')
-    final_translation = ' '.join(translation)        
-    print(final_translation)
- 
-morse_menu()
+    morse_translation = ' '.join(translation)        
+    return morse_translation
+
+if __name__ == "__main__":
+    morse_menu()
 
 
 #print(F'{text_to_morse(text)} /({text})')
